9951c7e1ae3ccf0e7322245784ea3a5eaa7dd3ab
Francelin <Ancelt65@gmail.com>
2021-09-06 09:52:49 +0800

    New commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
+import "@babel/polyfill";
+import "mutationobserver-shim";
 import Vue from "vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 new Vue({
