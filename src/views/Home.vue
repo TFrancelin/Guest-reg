e98cab0258e0f8ff9391ce60c1f1f5ab6024fb94
Steve Jones <jones.wiredbraincoffee@gmail.com>
2021-09-14 16:51:33 +0800

    n

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -58,17 +58,7 @@
                   ></el-input>
                 </el-form-item>
                 
-                 <div class="block">
-                  <p>Component value：{{ value }}</p>
-                 <el-date-picker
-                   v-model="value"
-                   type="daterange"
-                   start-placeholder="Start date"
-                   end-placeholder="End date"
-                   :default-time="['00:00:00', '23:59:59']">
-                  </el-date-picker>
-                </div>
-               </div>
+                 
                 <el-form-item label="Telephone" :label-width="formLabelWidth">
                   <el-input
                     v-model="form.telephone"
