fee0ae118dbc60eaeb093607ff7dc958d14ef93e
Francelin <Ancelt65@gmail.com>
2021-10-20 19:40:40 +0800

    Working on the forms validation

--- src/main.js
+++ src/main.js
@@ -9,9 +9,15 @@ import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Vuelidate from "vuelidate";
+
+axios.defaults.baseURL = "/api";
+Vue.config.productionTip = false;
+// Vue.prototype.axios = axios;
 
 Vue.use(VueAxios, axios);
 Vue.use(ElementUI);
+Vue.use(Vuelidate);
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -47,21 +47,33 @@
             <!-- Add Guest Button -->
             <el-button type="text" @click="add_guest_btn">添加客人</el-button>
             <!-- DialogBox with form in which to fill each user information -->
-            <el-dialog title="" :visible.sync="dialogFormVisible">
+            <el-dialog
+              title=""
+              :visible.sync="dialogFormVisible"
+              v-if="clicked == false"
+            >
               <h2 class="dialog_header" align="center">请添加你的信息</h2>
-              <el-form :model="form">
-                <el-form-item label="名字" :label-width="formLabelWidth">
+              <el-form :model="form" :rules="rules" ref="form">
+                <el-form-item
+                  label="名字"
+                  :label-width="formLabelWidth"
+                  prop="visitorName"
+                >
                   <el-input
                     v-model="form.visitorName"
                     autocomplete="on"
-                    clearable="true"
+                    clearable
                   ></el-input>
                 </el-form-item>
-                <el-form-item label="身份证号" :label-width="formLabelWidth">
+                <el-form-item
+                  label="身份证号"
+                  :label-width="formLabelWidth"
+                  prop="identification"
+                >
                   <el-input
                     v-model="form.identification"
                     autocomplete="on"
-                    clearable="true"
+                    clearable
                   ></el-input>
                 </el-form-item>
 
@@ -70,12 +82,13 @@
                     class="datetime"
                     label="开始时间"
                     :label-width="formLabelWidth"
+                    prop="startAt"
                   >
                     <el-date-picker
                       v-model="form.startAt"
                       type="datetime"
                       placeholder="选择日期和时间"
-                      clearable="true"
+                      clearable
                     >
                     </el-date-picker>
                   </el-form-item>
@@ -84,23 +97,27 @@
                     class="datetime"
                     label="结束时间"
                     :label-width="formLabelWidth"
+                    prop="endAt"
                   >
                     <el-date-picker
                       v-model="form.endAt"
                       type="datetime"
                       placeholder="选择日期和时间"
-                      clearable="true"
+                      clearable
                     >
                     </el-date-picker>
                   </el-form-item>
                 </div>
 
-                <el-form-item label="手机号码" :label-width="formLabelWidth">
+                <el-form-item
+                  label="手机号码"
+                  :label-width="formLabelWidth"
+                  prop="mobile"
+                >
                   <el-input
                     v-model.number="form.mobile"
                     autocomplete="off"
-                    type="number"
-                    clearable="true"
+                    clearable
                   ></el-input>
                 </el-form-item>
               </el-form>
@@ -108,7 +125,90 @@
               <!-- Cancel and confirm buttons at the dialogBox footer -->
               <span slot="footer" class="dialog-footer">
                 <el-button @click="cancel">取消</el-button>
-                <el-button type="primary" @click="add_guest(form)"
+                <el-button type="primary" @click="confirm(form)"
+                  >确认</el-button
+                >
+              </span>
+            </el-dialog>
+            <el-dialog
+              title=""
+              :visible.sync="dialogFormVisible"
+              v-if="clicked == true"
+            >
+              <h2 class="dialog_header" align="center">请修改你的信息</h2>
+              <el-form :model="form1" :rules="rules" ref="form1">
+                <el-form-item
+                  label="名字"
+                  :label-width="formLabelWidth"
+                  prop="visitorName"
+                >
+                  <el-input
+                    v-model="form1.visitorName"
+                    autocomplete="on"
+                    clearable
+                  ></el-input>
+                </el-form-item>
+                <el-form-item
+                  label="身份证号"
+                  :label-width="formLabelWidth"
+                  prop="identification"
+                >
+                  <el-input
+                    v-model="form1.identification"
+                    autocomplete="on"
+                    clearable
+                  ></el-input>
+                </el-form-item>
+
+                <div class="datepicker">
+                  <el-form-item
+                    class="datetime"
+                    label="开始时间"
+                    :label-width="formLabelWidth"
+                    prop="startAt"
+                  >
+                    <el-date-picker
+                      v-model="form1.startAt"
+                      type="datetime"
+                      placeholder="选择日期和时间"
+                      clearable
+                    >
+                    </el-date-picker>
+                  </el-form-item>
+
+                  <el-form-item
+                    class="datetime"
+                    label="结束时间"
+                    :label-width="formLabelWidth"
+                    prop="endAt"
+                  >
+                    <el-date-picker
+                      v-model="form1.endAt"
+                      type="datetime"
+                      placeholder="选择日期和时间"
+                      clearable
+                    >
+                    </el-date-picker>
+                  </el-form-item>
+                </div>
+
+                <el-form-item
+                  label="手机号码"
+                  :label-width="formLabelWidth"
+                  prop="mobile"
+                >
+                  <el-input
+                    v-model.number="form1.mobile"
+                    autocomplete="off"
+                    clearable
+                  ></el-input>
+                </el-form-item>
+              </el-form>
+
+              <!-- Cancel and confirm buttons at the dialogBox footer -->
+              <span slot="footer" class="dialog-footer">
+                <el-button @click="cancel">取消</el-button>
+                <el-button type="primary" @click="confirm(form1)"
                   >确认</el-button
                 >
               </span>
@@ -173,12 +273,14 @@
 </template>
 
 <script>
+// import { required, minLength } from "vuelidate/lib/validators";
 // @ is an alias to /src
 export default {
   name: "Home",
   components: {},
   data() {
     return {
+      clicked: false,
       search: "",
       users: [],
       dialogFormVisible: false,
@@ -222,15 +324,35 @@ export default {
       },
       formLabelWidth: "140px",
       checkInRecord: "",
+      rules: {
+        mobile: [
+          { min: 11, max: 11, message: "手机号不能小或大于11位", trigger: "blur" },
+        ],
+        identification: [
+          {
+            required: true,
+            message: "身份证号不能为空",
+            trigger: ["blur", "change"],
+          },
+          {
+            type: "tel",
+            message: "身份证号不能小于或大于18位",
+            trigger: ["blur", "change"],
+          },
+        ],
+      },
     };
   },
 
-  validations: {
-    identification: {
-      required: true,
-      minLength: 18,
-    },
-  },
+  // validations: {
+  //   visitorName: {
+  //     required,
+  //   },
+  //   identification: {
+  //     required,
+  //     minLength: minLength(18),
+  //   },
+  // },
 
   created() {
     this.initData();
@@ -243,7 +365,7 @@ export default {
       // that.startAt = new Date('2014-05-10 13:25:50').getTime();
       // console.log(that.startAt);
       axios
-        .get("http://60.205.247.119:8080/visit-sys/visitor/list", {})
+        .get("/visitor/list", {})
         .then(function (response) {
           console.log(response);
           if (response.data.code == 10001) {
@@ -268,47 +390,104 @@ export default {
       this.dialogFormVisible = true;
     },
 
-    add_guest() {
+    confirm(element) {
       console.log(this.form);
       const axios = require("axios");
       let that = this;
-      that.form.startAt = new Date(this.form.startAt).getTime();
-      console.log(that.form.startAt);
-      that.form.endAt = new Date(this.form.endAt).getTime();
-      axios
-        .post("http://60.205.247.119:8080/visit-sys/visitor/save", {
-          identification: that.form.identification,
-          startAt: that.form.startAt,
-          endAt: that.form.endAt,
-          mobile: that.form.mobile,
-          approver: "",
-          approverId: "",
-          content: "",
-          createdAt: 0,
-          face_token: "",
-          faceset: "",
-          id: "",
-          imgUrl: "",
-          staff: "",
-          staffId: "",
-          visitorCompany: "",
-          visitorName: that.form.visitorName,
-        })
-        .then((response) => {
-          console.log(response);
-          console.log(that.form);
-          that.users.concat(that.users.push(this.form));
-          // that.users = that.users.concat(response.data.data); //Users array already exist with data in, so it'll add new data in and insert in the database on the server side
-          that.initData();
-        })
-        .catch(function (error) {
-          console.log(error);
+      if (element == this.form) {
+        console.log("Element is equal to form");
+        let times = new Date(this.form.startAt).getTime();
+        that.form.startAt = times / 1000;
+        console.log(that.form.startAt);
+        let times1 = new Date(this.form.endAt).getTime();
+        that.form.endAt = times1 / 1000;
+
+        this.$refs.form.validate((valid) => {
+          if (valid) {
+            axios
+              .post("/visitor/save", {
+                identification: that.form.identification,
+                startAt: that.form.startAt,
+                endAt: that.form.endAt,
+                mobile: that.form.mobile,
+                approver: "",
+                approverId: "",
+                content: "",
+                createdAt: 0,
+                face_token: "",
+                faceset: "",
+                id: "",
+                imgUrl: "",
+                staff: "",
+                staffId: "",
+                visitorCompany: "",
+                visitorName: that.form.visitorName,
+              })
+              .then((response) => {
+                console.log(response);
+                console.log(that.form);
+                that.users.concat(that.users.push(this.form)); //Users array already exist with data in, so it'll add new data in and insert in the database on the server side
+                that.initData();
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+            this.dialogFormVisible = false;
+          } else {
+            console.log("error submit!!");
+            return false;
+          }
         });
-      this.dialogFormVisible = false;
+      } else if (element == this.form1) {
+        let that = this;
+        // const axios = require("axios");
+        console.log('We"re editing');
+        let times = new Date(this.form1.startAt).getTime();
+        that.form1.startAt = times / 1000;
+        console.log(that.form1.startAt);
+        let times1 = new Date(this.form1.endAt).getTime();
+        that.form1.endAt = times1 / 1000;
+
+        this.$refs.form1.validate((valid) => {
+          if (valid) {
+            axios
+              .put("/visitor/update", {
+                identification: that.form1.identification,
+                startAt: that.form1.startAt,
+                endAt: that.form1.endAt,
+                mobile: that.form1.mobile,
+                approver: that.form1.approver,
+                approverId: that.form1.approverId,
+                content: that.form1.content,
+                createdAt: that.form1.createdAt,
+                face_token: that.form1.face_token,
+                faceset: that.form1.faceset,
+                id: that.form1.id,
+                imgUrl: that.form1.imgUrl,
+                staff: that.form1.staff,
+                staffId: that.form1.staffId,
+                visitorCompany: that.form1.visitorCompany,
+                visitorName: that.form1.visitorName,
+              })
+              .then(function (response) {
+                console.log(response);
+                // that.form = that.form1;
+                // console.log(that.form);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+            this.dialogFormVisible = false;
+          } else {
+            console.log("error submit!!");
+            return false;
+          }
+        });
+      }
     },
 
     cancel() {
-      this.form = this.form1;
+      // this.form = this.form1;
       this.dialogFormVisible = false;
     },
 
@@ -321,37 +500,11 @@ export default {
     // },
 
     edit_guest(user) {
+      this.clicked = true;
+      this.form1 = user;
       this.dialogFormVisible = true;
-      this.form = user;
       // console.log(this.form);
-      // console.log(user);
-      let that = this;
-      const axios = require("axios");
-      axios
-        .put("http://60.205.247.119:8080/visit-sys/visitor/update", {
-          identification: that.form.identification,
-          startAt: that.form.startAt,
-          endAt: that.form.endAt,
-          mobile: that.form.mobile,
-          approver: "",
-          approverId: "",
-          content: "",
-          createdAt: 0,
-          face_token: "",
-          faceset: "",
-          id: "",
-          imgUrl: "",
-          staff: "",
-          staffId: "",
-          visitorCompany: "",
-          visitorName: that.form.visitorName,
-        })
-        .then(function (response) {
-          console.log(response);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+      console.log(user);
     },
 
     deleteRow(index, user) {
@@ -359,9 +512,7 @@ export default {
       let that = this;
       console.log(user);
       axios
-        .delete(
-          "http://60.205.247.119:8080/visit-sys/visitor/del?id=" + user.id
-        )
+        .delete("/visitor/del?id=" + user.id)
         .then(function (response) {
           console.log(response);
           that.users.splice(index, 1);
